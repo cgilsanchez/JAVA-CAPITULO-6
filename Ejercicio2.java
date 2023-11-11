c67c7698ad0103e8d3ddf73481d5018edb7d86eb
cgilsanchez <cgilsanchez.02@gmail.com>
2023-11-11 12:11:25 +0100

    first commit

---
+++ Ejercicio2.java
@@ -0,0 +1,22 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int palos = (int)(Math.random()*4+1);
+        switch (palos) {
+            case 1:
+                
+            break;
+            case 2:
+                
+            break;
+            case 3:
+                
+            break;
+            case 4:
+                
+            break;
+
+            default:
+                break;
+        }
+    }
+}

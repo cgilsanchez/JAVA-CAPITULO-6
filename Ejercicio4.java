cb6b5b09c9a73fb2c645d2f96b5322a89a70aa73
cgilsanchez <cgilsanchez.02@gmail.com>
2023-11-14 20:02:06 +0100

    first commit

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,3 +1,9 @@
 public class Ejercicio4 {
-    
+    public static void main(String[] args) {
+        System.out.println("Los 20 numeros aleatorios entre 0-10 son: ");
+        for (int i = 0; i < 20; i++) {
+            int numero = (int)(Math.random()*11);
+            System.out.print(numero + "  ");
+        }
+    }
 }

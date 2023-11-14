cab73fafc7c44da85f74a91e4de261c89f89e591
cgilsanchez <cgilsanchez.02@gmail.com>
2023-11-14 20:02:23 +0100

    first commit

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -1,3 +1,28 @@
 public class Ejercicio6 {
+    public static void main(String[] args) {
+        int n = (int)(Math.random()*100);
+        System.out.print("Intenta adivinar el numero: ");
+        int contador = 4;
+        
+        for (int i = 0; i <5; i++) {
+            int n1 = Integer.parseInt(System.console().readLine());
+            if (n>n1) {
+                System.out.println("El numero secreto es mayor");
+                System.out.println("Te quedan "+ contador + " intentos") ;
+            }else if (n<n1) {
+                System.out.println("El numero secreto es menor");
+                System.out.println("Te quedan "+ contador + " intentos") ;
+            }
+            
+            if (n==n1) {
+                System.out.println("Has acertado el numero , Enhorabuena");
+                break;
+            }
+            contador--;
+        }
+            
+        
+            }
+        }
     
-}
+

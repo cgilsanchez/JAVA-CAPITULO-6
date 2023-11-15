76845aa8e7f8263aa1d1a1f789f41243a53e3dd8
cgilsanchez <cgilsanchez.02@gmail.com>
2023-11-15 12:36:31 +0100

    first commit

--- Ejercicio9.java
+++ Ejercicio9.java
@@ -1,3 +1,15 @@
 public class Ejercicio9 {
-    
+    public static void main(String[] args) {
+        int n = 0;
+        int contador = 0;
+        while (n!=24) {
+            n= (int)(Math.random()*101);
+                if (n%2==0) {
+                    System.out.print(n + " ");
+                }
+            contador++;
+        }
+        System.out.println();
+        System.out.println("Se han generado "+ contador + " numeros");
+    }
 }

3270148d97c0aafdedcd40ae25c38d4588d42e22
cgilsanchez <cgilsanchez.02@gmail.com>
2023-11-15 12:36:44 +0100

    first commit

--- Ejercicio13.java
+++ Ejercicio13.java
@@ -1,3 +1,13 @@
 public class Ejercicio13 {
-    
+    public static void main(String[] args) {
+        int n = 0;
+        int n1 = 1;
+        while (n!=n1) {
+            n = (int)(Math.random()*6+1);
+            n1 = (int)(Math.random()*6+1);
+            System.out.println("Primer dado : "+ n);
+            System.out.println("Segundo dado : "+ n1);
+            System.out.println();
+        }
+    }
 }

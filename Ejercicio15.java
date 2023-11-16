f419e6e08f77f7c3d526755749a431cd7ddb07cd
cgilsanchez <cgilsanchez.02@gmail.com>
2023-11-16 19:18:22 +0100

    first commit

--- Ejercicio15.java
+++ Ejercicio15.java
@@ -1,5 +1,9 @@
 public class Ejercicio15 {
     public static void main(String[] args) {
-        
+        int contador = 4;
+        int n = 0;
+        for (int i = 0; i < 5; i++) {
+            
+        }
     }
 }
